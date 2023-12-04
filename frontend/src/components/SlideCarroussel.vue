5df013f4d815a6606ca14d5ba066d97a84ce43ce
Lucas Bedate <lucasbedate2004@gmail.com>
2023-12-03 23:37:57 -0300

    segundo commit

---
+++ frontend/src/components/SlideCarroussel.vue
@@ -0,0 +1,20 @@
+<template>
+    <img class="slidecarrousel-icon" alt="" src="SlideCarrousel.png" />
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    
+    
+    export default defineComponent({
+          name: "SlideCarrouselImage"})
+          
+</script>
+          
+<style scoped>.slidecarrousel-icon {
+          position: relative;
+          width: 100%;
+          height: 649px;
+          object-fit: cover;
+    }
+    
+    </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,21 +1,14 @@
 <script setup>
+import NavBar from '../components/NavBar.vue';
+import SlideCarroussel from '../components/SlideCarroussel.vue';
 </script>
 
 <template>
-    <section class="container text-black">
-        <h1 class="text-8xl">Box2box</h1>
-    </section>
+<div>teste</div>
+<NavBar />
+<SlideCarroussel />
 </template>
 
 <style scoped>
-section {
-    height: 100%;
-}
 
-h1 {
-    text-transform: uppercase;
-    letter-spacing: 30px;
-    font-weight: bold;
-    text-align: center;
-}
 </style>
